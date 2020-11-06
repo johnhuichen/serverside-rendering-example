1db1045ddbc6a968da0d1fedf63f086a57f1b8c7
John Chen <johnhuichen@gmail.com>
2020-11-06 17:21:02 -0500

    Update code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
+
+ReactDom.render(
+    <Route />
+  document.getElementById("root")
+);
